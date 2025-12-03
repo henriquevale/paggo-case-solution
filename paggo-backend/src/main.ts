a4af2a96ef3f2715f12aa269b64e24fa2778a5ad
henriquevale <38165820+henriquevale@users.noreply.github.com>
2025-12-03 14:27:25 -0300

    Update main.ts

--- paggo-backend/src/main.ts
+++ paggo-backend/src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // <--- Isso libera o acesso
-  await app.listen(3000);
+  
+  // Habilita CORS para aceitar conexões do Frontend (Vercel)
+  app.enableCors();
+  
+  // CORREÇÃO CRÍTICA DO RENDER:
+  // Usa a variável PORT do sistema (Render) OU 3000 (Local)
+  // '0.0.0.0' é necessário para aceitar conexões externas no Render
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
