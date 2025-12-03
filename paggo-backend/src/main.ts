de8bc4817d3c0457a157e430e501d653b5d0780d
henriquevale <38165820+henriquevale@users.noreply.github.com>
2025-12-03 12:16:47 -0300

    Update main.ts

--- paggo-backend/src/main.ts
+++ paggo-backend/src/main.ts
@@ -3,8 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  
+  app.enableCors(); // <--- Isso libera o acesso
   await app.listen(3000);
 }
 bootstrap();
